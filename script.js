ad3ed77775908de51374e6440c9414b870381c1f
Jaine09 <jainejesus0911@gmail.com>
2025-04-14 11:29:04 -0300

    atualizado

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ viloes = ["","",""]; //escolha do computador
 
 forcaPersonagem = 0;
 forcaVilao = 0;
-alert("O array personagem contém: " + personagem);
+
 for(let i = 0; i<3; i++){
     escolhaPersonagem = prompt("Digite o nome do seu personagem " + (i+1));
     personagem[i] = escolhaPersonagem;
